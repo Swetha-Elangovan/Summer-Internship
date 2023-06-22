8ca9527e60dc0d672cf1a6824bbf1bada5b13f4f
SWETHA E <106983922+SwethaE-CSE@users.noreply.github.com>
2023-06-22 12:47:37 +0530

    Destructuring in JS

--- Destructuring.js
+++ Destructuring.js
@@ -1,4 +1,6 @@
 //De-structuring - Unpacking
+
+//array destructuring
 let arrnum=[10,20,30,40,50,60,70];
 let [a, b, c, d, e] = arrnum;
 console.log(e)
@@ -6,3 +8,19 @@ console.log(e)
 let arrnum1=[10,20,30,40,50,60,70,80];
 let [a1, b1, , c1, d1, ...e1] = arrnum1; // , , - skip a element & ...e1-rest operator
 console.log(e1)
+
+//object destructuring
+const o1={
+    nam:"sece",
+    year:2021,
+    year:2025,
+    city:"CBE",
+    fees:5000,
+    color:"Blue"
+};
+
+let {nam: clgname, year, city, ...anything}=o1;
+console.log(clgname, year, city, anything);
+
+let {nam: clgname, year, city, color="green", size="829"}=o1;
+console.log(clgname, year, city, color, size);
