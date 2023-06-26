99930adfefc15b2f98313b346c2ccd2a5eca6942
SWETHA E <106983922+SwethaE-CSE@users.noreply.github.com>
2023-06-26 09:35:57 +0530

    Installing Packages

---
+++ packages.js
@@ -0,0 +1,13 @@
+npm init
+package name: (bigproject) color-generator
+version: (1.0.0)                                                                                                          
+description: To generate random color                                                                                     
+entry point: (index.js)                                                                                                   
+test command:                                                                                                             
+git repository:                                                                                                           
+keywords: learn npm                                                                                                       
+author: swetha                                                                                                            
+license: (ISC)        
+
+npm install express
+added 58 packages, and audited 59 packages in 10s
