f0c27a066a72983bee4b209cbb5bc029a1a5e0eb
SWETHA E <106983922+SwethaE-CSE@users.noreply.github.com>
2023-06-26 11:17:39 +0530

    Accessing JSON file in NodeJS

--- AccessingJsonFile.js
+++ AccessingJsonFile.js
@@ -4,3 +4,12 @@ console.log(info);
 console.log(info.Name);
 console.log(info.Address);
 console.log(info.Address.District);
+
+const k = Object.keys(info);
+for(let i=0;i<k.length;i++){
+    const val = k[i];
+    if (typeof info[val] === 'object')
+    {
+        console.log(val, info[val]);
+    } 
+}
